35034fb2b0b8a724ed7a027b260a36b2ae291f6b
jozadaque <jozadaquesousa@gmail.com>
2022-04-13 21:44:25 -0300

    feat:
    MenuTop Add

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -15,10 +15,69 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.white,
         elevation: 0,
         title: const Text(
-          'News',
+          'News 24',
           style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
+      ),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  ItemMenuTop(
+                    title: "Top",
+                    contentColor: Colors.black,
+                    textColor: Colors.white,
+                  ),
+                  ItemMenuTop(title: 'World'),
+                  ItemMenuTop(title: 'Politic'),
+                  ItemMenuTop(title: 'Entertainment')
+                ],
+              ),
+            ),
+            Column(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemMenuTop extends StatelessWidget {
+  const ItemMenuTop({
+    Key? key,
+    required this.title,
+    this.textColor = Colors.black38,
+    this.contentColor = Colors.black12,
+  }) : super(key: key);
+
+  final String title;
+  final Color textColor;
+  final Color contentColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: contentColor,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+          child: Text(
+            title,
+            textAlign: TextAlign.center,
+            style: TextStyle(color: textColor, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
     );
   }
